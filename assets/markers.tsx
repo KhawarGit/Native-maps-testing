c02f5e4228f0bbe78d141da40665a3df3f35f2dc
Khawar Khan <ma3533899@gmail.com>
2025-05-14 07:45:46 +0500

    added map view with markers file

---
+++ assets/markers.tsx
@@ -0,0 +1,13 @@
+const markers = [
+    {
+        "name": "Beach",
+        "coordinates": {
+            "latitude": 17.6868,
+            "longitude": 83.2185,
+            "latitudeDelta": 0.1,
+            "longitudeDelta": 0.1
+        },
+        "description": "rendering maps with react-native-maps"
+    }
+];
+export default markers;
