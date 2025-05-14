a459a4ce13858ece35ec92d23a737c6e0a07f765
Khawar Khan <ma3533899@gmail.com>
2025-05-14 08:17:04 +0500

    added more markers

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import markers from '@/assets/markers';
 export default function App() {
@@ -7,8 +7,14 @@ export default function App() {
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        initialRegion={markers[0].coordinates}
-      />
+        initialRegion={markers[0].coordinates}>
+        {markers.map((marker, index) => (<Marker
+          key={index}
+          title={marker.name}
+          coordinate={marker.coordinates}
+          />
+        ))}    
+      </MapView>
     </View>
   );
 }
--- assets/markers.tsx
+++ assets/markers.tsx
@@ -8,6 +8,16 @@ const markers = [
             "longitudeDelta": 0.1
         },
         "description": "rendering maps with react-native-maps"
-    }
+    },
+    {
+        "name": "Food Savers",
+        "coordinates": {
+            "latitude": 24.92165697554529,
+            "longitude": 67.25060882044119,
+            "latitudeDelta": 0.0043,
+            "longitudeDelta": 0.0034
+        },
+        "description": "Figh agains Hunger"
+    }, 
 ];
 export default markers;
